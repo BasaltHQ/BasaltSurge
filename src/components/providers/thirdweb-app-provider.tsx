7551cbc0ce178ccbe3782a0208073fc08dfea2ab
GenRevo89 <krishnapatel@mac.com>
2026-06-29 16:57:58 -0600

    v1.8.2a - Build errors patched.

--- src/components/providers/thirdweb-app-provider.tsx
+++ src/components/providers/thirdweb-app-provider.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React, { useState, useEffect } from "react";
 import { ThirdwebProvider } from "thirdweb/react";
 import { getResolvedClientId } from "@/lib/thirdweb/client";
